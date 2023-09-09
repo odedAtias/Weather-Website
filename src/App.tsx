a4a96f9842d6e1d777aaf5962a8a57ea6b6004b6
Oded <oatias@baybridgedigital.com>
2023-09-09 11:34:12 +0300

    Installing bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ const App = () => {
   return (
     <>
       <h1>Hello From React</h1>
+      <a className="btn btn-primary"
+        data-bs-toggle="collapse"
+        href="#collapseExample"
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapseExample">
+        Bootstrap button
+      </a>
     </>
   )
 }
